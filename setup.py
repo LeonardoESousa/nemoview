03691626b747c7826f1e8e1958cfd29a03b437ec
LeonardoESousa <leonardo.sousa137@gmail.com>
2023-06-17 19:59:03 +0200

    now compatible with voila 0.4 and ipywidgets 8

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ REQUIRES_PYTHON = '>=3.6.0'
 VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
-REQUIRED = ['matplotlib<=3.5.0', 'voila==0.3.2', 'ipywidgets==7.7.2', 'pandas==1.5.2', 'IPython','Ipympl==0.7.0', 'jinja2==3.0.0','nemophoto>=0.6.0']
+REQUIRED = ['matplotlib<=3.5.0', 'voila==0.4.0', 'ipywidgets==8.0.6', 'pandas==1.5.2', 'IPython','Ipympl==0.7.0', 'jinja2==3.0.0','nemophoto>=0.6.0']
 
 # What packages are optional?
 EXTRAS = {
