cb10e52de69c15caac6463e8b33ce72f71218da9
LeonardoESousa <leonardo.sousa137@gmail.com>
2023-10-17 11:19:25 +0200

    linting

--- nemoview/__main__.py
+++ nemoview/__main__.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-   
+
 def main():
     path = os.path.join(os.path.dirname(__file__),"nemodash.ipynb")
     path = r"{} ".format(path)
@@ -11,9 +11,8 @@ def main():
         option = r'--Voila.tornado_settings={}'.format('''{'websocket_max_message_size': 209715200}''')
         option += ''' --VoilaConfiguration.file_whitelist="['figs/favicon.ico']"'''
         subprocess.check_output(['voila',path,option])
-    except subprocess.CalledProcessError as e:
+    except subprocess.CalledProcessError:
         option =  " --Voila.tornado_settings \"'websocket_max_message_size'=209715200\""
         option += ''' --VoilaConfiguration.file_whitelist="['figs/favicon.ico']"'''
         command = "voila "+path+option
-        subprocess.run(command, shell=True)     
-
+        subprocess.run(command, shell=True)
